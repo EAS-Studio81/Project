c37f502df2a2433d4c7702edc5b66959566bd52c
EAS-Studio81 <evanditya.arziqi9@smp.belajar.id>
2025-02-19 19:44:48 +0700

    Add files via upload

---
+++ guess-the-world/word-db.js
@@ -0,0 +1,39 @@
+// While making answer, do not enter a same letter example : "banana, java etc."
+let wordList = [
+    {
+        word: "candy",
+        hint: "sweet treat"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "a image file format"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "mobile",
+        hint: "an electronic device"
+    },
+    {
+        word: "gpu",
+        hint: "computer component"
+    },
+    {
+        word: "lua",
+        hint: "programming language"
+    },
+    {
+        word: "cinema",
+        hint: "a theater film"
+    },
+    {
+        word: "ai",
+        hint: "a smart technology..."
+    },
+]
